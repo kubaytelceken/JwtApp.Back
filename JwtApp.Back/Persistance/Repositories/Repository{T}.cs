d82eae5fdb824af97074762e2392f42bb5e80a00
kubaytelceken <kubaytelceken@outlook.com>
2023-01-01 00:22:31 +0300

    Proje dosyası ekle.

---
+++ JwtApp.Back/Persistance/Repositories/Repository{T}.cs
@@ -0,0 +1,50 @@
+﻿using JwtApp.Back.Core.Application.Interfaces;
+using JwtApp.Back.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace JwtApp.Back.Persistance.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly JwtContext jwtContext;
+        public Repository(JwtContext jwtContext)
+        {
+            this.jwtContext = jwtContext;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await jwtContext.Set<T>().AddAsync(entity);
+            await jwtContext.SaveChangesAsync() ; 
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await jwtContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await jwtContext.Set<T>().AsNoTracking().SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await jwtContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            jwtContext.Set<T>().Remove(entity);
+            await jwtContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            jwtContext.Set<T>().Update(entity);
+            await jwtContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ JwtApp.Back/Program.cs
@@ -0,0 +1,32 @@
+using JwtApp.Back.Core.Application.Interfaces;
+using JwtApp.Back.Persistance.Context;
+using JwtApp.Back.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<JwtContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
+});
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
