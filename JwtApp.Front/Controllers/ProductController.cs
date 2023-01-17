859c8de9c21bb277eff014a5a6470c5812755853
kubaytelceken <kubaytelceken@outlook.com>
2023-01-17 22:03:44 +0300

    Create Update Product

--- JwtApp.Front/Controllers/ProductController.cs
+++ JwtApp.Front/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using JwtApp.Front.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Text;
 using System.Text.Json;
 
 namespace JwtApp.Front.Controllers
@@ -51,6 +54,155 @@ namespace JwtApp.Front.Controllers
             }
 
             return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = new CreateProductModel();
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var client = this.httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                string url = GenericClass.BaseAdress + "/api/Categories/getCategories";
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    ViewBag.Categories = result;
+                    model.Categories = new SelectList(result, "Id", "Definition") ;
+
+
+                    return View(model);
+                }
+
+            }
+            return RedirectToAction("List");
+            
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductModel model)
+        {
+            var data = TempData["Categories"]?.ToString();
+            if (data != null)
+            {
+                var categories = JsonSerializer.Deserialize<List<SelectListItem>>(data);
+                model.Categories = new SelectList(categories, "Value", "Text");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+                if (token != null)
+                {
+                    var client = this.httpClientFactory.CreateClient();
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+                    var jsonData = JsonSerializer.Serialize(model);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                    string url = GenericClass.BaseAdress + "/api/products";
+                    var response = await client.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("List");
+                    }
+                    ModelState.AddModelError("", "Bir hata oluştu");
+                }
+
+            }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var client = this.httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                string url = GenericClass.BaseAdress + "/api/products/" + id;
+                var responseProduct = await client.GetAsync(url);
+
+                if (responseProduct.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseProduct.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<UpdateProductModel>(jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+
+
+                    url = GenericClass.BaseAdress + "/api/categories/getCategories";
+                    var responseCategory = await client.GetAsync(url);
+
+                    if (responseCategory.IsSuccessStatusCode)
+                    {
+                        var jsonCategoryData = await responseCategory.Content.ReadAsStringAsync();
+
+                        var data = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonCategoryData, new JsonSerializerOptions
+                        {
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                        });
+
+                        if (result != null)
+                            result.Categories = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(data, "Id", "Definition");
+                    }
+
+
+
+                    return View(result);
+                }
+            }
+
+            return RedirectToAction("List");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateProductModel model)
+        {
+            var data = TempData["Categories"]?.ToString();
+            if (data != null)
+            {
+                var categories = JsonSerializer.Deserialize<List<SelectListItem>>(data);
+                model.Categories = new SelectList(categories, "Value", "Text", model.CategoryId);
+            }
+
+
+            if (ModelState.IsValid)
+            {
+
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+                if (token != null)
+                {
+                    var client = this.httpClientFactory.CreateClient();
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+                    var jsonData = JsonSerializer.Serialize(model);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    var url = GenericClass.BaseAdress + "/api/products";
+                    var response = await client.PutAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("List");
+                    }
+                    ModelState.AddModelError("", "Bir hata oluştu");
+                }
+
+            }
+            return View(model);
         }
     }
 }
---
+++ JwtApp.Front/Models/CreateProductModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace JwtApp.Front.Models
+{
+    public class CreateProductModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Stock is required.")]
+        public int Stock { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "CategoryId is required.")]
+        public int CategoryId { get; set; }
+        public SelectList? Categories { get; set; }
+    }
+}
